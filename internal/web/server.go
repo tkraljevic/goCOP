a36cb2b5bf7bda3b6202c780519ae028145c4e27
Tomislav Kraljević <78041730+tkraljevic@users.noreply.github.com>
2026-09-17 22:54:11 +0200

    Katalog vrsta sredstava uređuje se na ekranu
    Stranica Katalog vrsta pokazuje propisani popis po skupinama, i ugašene
    vrste. Uprava sektora i administrator dodaju novu vrstu (naziv, skupina,
    jedinica, vodi li se prazno i napunjeno); ide na kraj svoje skupine i
    odmah se nudi u obrascu zahvata. Vrsta se ne briše nego gasi, jer stari
    promet i popisi na nju pokazuju.

--- internal/web/server.go
+++ internal/web/server.go
@@ -498,7 +498,7 @@ func NewServer(
 	for _, page := range []string{"dashboard.html", "registri.html", "users.html", "user_detail.html", "user_form.html", "duty_form.html", "profile.html", "sections.html", "section_detail.html", "section_form.html", "territories.html", "county_form.html", "municipality_form.html", "municipality_detail.html", "stations.html", "station_detail.html", "station_form.html", "station_history.html", "station_history_form.html", "paket_pregled.html", "watercourses.html", "watercourse_detail.html", "watercourse_form.html", "structures.html", "structure_detail.html", "structure_form.html", "readings.html", "reading_history.html", "reading_form.html", "arhiva_ispravci.html", "uvoz_ocitanja.html", "teren.html", "moduli.html", "settings.html", "odrzavanje.html", "organizacija.html", "sector_form.html", "area_form.html", "contractor_form.html", "firme.html", "nazivi.html", "sudionici.html",
 		"administracija.html", "uvozi.html", "sinkronizacija.html", "pretplate.html", "baza.html", "izvori.html", "uvoz_niza.html",
 		"dnevnici.html", "dnevnici_izbor.html", "dnevnik_form.html", "dnevnik.html", "dnevnik_cop.html", "dnevnik_cop_form.html", "dnevnik_list.html", "dnevnik_obracun.html", "dnevnik_dezurstva.html", "dnevnik_iors.html", "izvjesca.html", "izvjesce_form.html", "izvjesce.html", "sektorsko_form.html", "sektorsko.html", "obracun_postavke.html",
-		"sredstva.html", "skladiste.html", "skladiste_form.html", "promet_form.html", "promet.html", "gdje_ima.html", "na_terenu.html", "popisi.html", "popis_form.html", "popis.html", "pomoc.html", "ocitanja_ispravci.html"} {
+		"sredstva.html", "katalog.html", "skladiste.html", "skladiste_form.html", "promet_form.html", "promet.html", "gdje_ima.html", "na_terenu.html", "popisi.html", "popis_form.html", "popis.html", "pomoc.html", "ocitanja_ispravci.html"} {
 		t, err := template.New("base.html").Funcs(tmplFuncs).ParseFS(templatesFS, DijeloviPredloska(page)...)
 		if err != nil {
 			return nil, fmt.Errorf("greška pri parsiranju predloška %s: %w", page, err)
@@ -912,6 +912,9 @@ func (s *Server) setupRoutes() {
 	s.mux.Handle("GET /sredstva", s.authMiddleware(http.HandlerFunc(mtsH.ShowPregled)))
 	s.mux.Handle("GET /sredstva/promet", s.authMiddleware(http.HandlerFunc(mtsH.ShowPromet)))
 	s.mux.Handle("GET /sredstva/mts.xlsx", s.authMiddleware(http.HandlerFunc(mtsH.IzvoziTablicu)))
+	s.mux.Handle("GET /sredstva/katalog", s.authMiddleware(http.HandlerFunc(mtsH.ShowKatalog)))
+	s.mux.Handle("POST /sredstva/katalog", s.authMiddleware(http.HandlerFunc(mtsH.HandleSaveVrsta)))
+	s.mux.Handle("POST /sredstva/katalog/{id}", s.authMiddleware(http.HandlerFunc(mtsH.HandleSaveVrsta)))
 	s.mux.Handle("GET /sredstva/na-terenu", s.authMiddleware(http.HandlerFunc(mtsH.ShowNaTerenu)))
 	s.mux.Handle("GET /sredstva/gdje/{vrsta}", s.authMiddleware(http.HandlerFunc(mtsH.ShowGdjeIma)))
 	s.mux.Handle("GET /sredstva/skladista/novo", s.authMiddleware(http.HandlerFunc(mtsH.ShowSkladisteForm)))
